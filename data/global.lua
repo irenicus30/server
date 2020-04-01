63ccd2208bfbcde3fe6196c5b6b6b0e4905cb31b
Marcin Sobczyk <irenicus30@gmail.com>
2020-04-02 00:17:53 +0200

    fix looting

--- data/global.lua
+++ data/global.lua
@@ -115,6 +115,31 @@ function getTibianTime()
 	return hours .. ':' .. minutes
 end
 
+function getDistanceBetween(firstPosition, secondPosition)
+	local xDif = math.abs(firstPosition.x - secondPosition.x)
+	local yDif = math.abs(firstPosition.y - secondPosition.y)
+	local posDif = math.max(xDif, yDif)
+	if firstPosition.z ~= secondPosition.z then
+		posDif = posDif + 15
+	end
+	return posDif
+end
+
+function getFormattedWorldTime()
+	local worldTime = getWorldTime()
+	local hours = math.floor(worldTime / 60)
+
+	local minutes = worldTime % 60
+	if minutes < 10 then
+		minutes = '0' .. minutes
+	end
+	return hours .. ':' .. minutes
+end
+
+function getLootRandom()
+	return math.random(0, MAX_LOOTCHANCE) / configManager.getNumber(configKeys.RATE_LOOT)
+end
+
 -- Stamina
 if nextUseStaminaTime == nil then
 	nextUseStaminaTime = {}
